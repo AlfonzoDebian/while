677d06a66a64fd019db0aa1e75ca9e21266b7785
AlfonzoDebian <luismarquez@colegioguanenta.edu.co>
2025-02-13 18:24:41 -0500

    sigma

--- n.py
+++ n.py
@@ -2,11 +2,14 @@
 n = int(input("Introducir el valor n: "))
 s = 0
 i = 1
+
 #Processing
 while i <= n:
     s = s + i
     i = i + 1
-    #Output
-    print("La suma de los: " + str(n) + "El valor: " + str(s))
 pass
+
+#output
+print("La suma de los: " + str(n) + "El valor: " + str(s))
+
 
